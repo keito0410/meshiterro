67631f7f976592ae8b8cadf5712d5ce94ba7ee38
keito0410 <041019keito@gmail.com>
2020-04-29 23:51:30 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '42cc27a04592c5788193d79fc6b4919ee85c53beb9ae8c5c263e1627eb93ca40403691df6eb9d77496e44d8190a92603b32d6a91c6da9bbbcb6c0c1ac316614e'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show]
+    devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
