480a73d88c3719b2dae41fe6098da4289443c26f
keito0410 <041019keito@gmail.com>
2020-04-30 21:59:40 +0900

    [Finish] 5-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :edit, :update]
   root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
   end
-    resources :users, only: [:show]
     devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
